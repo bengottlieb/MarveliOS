0eda8f56408fd157c0d1573e62bc5fc0ba8545d0
Ben Gottlieb <ben@standalone.com>
2014-04-26 05:11:03 -0500

    Initial Commit

---
+++ MarveliOS/MV_AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MV_AppDelegate.h
+//  MarveliOS
+//
+//  Created by Ben Gottlieb on 4/26/14.
+//  Copyright (c) 2014 Stand Alone, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MV_AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MarveliOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MarveliOS
+//
+//  Created by Ben Gottlieb on 4/26/14.
+//  Copyright (c) 2014 Stand Alone, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MV_AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([MV_AppDelegate class]));
+	}
+}
