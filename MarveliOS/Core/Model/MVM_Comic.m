69dd39629e4b07c668f048e086d9e6dc6444da7e
Ben Gottlieb <ben@standalone.com>
2014-05-04 21:50:50 -0500

    changed default sortKey for comics

--- MarveliOS/Core/Model/MVM_Comic.m
+++ MarveliOS/Core/Model/MVM_Comic.m
@@ -11,7 +11,7 @@
 @implementation MVM_Comic
 
 - (MV_Object_type) objectType { return MV_Object_type_comic; }
-+ (NSString *) defaultSortKey { return @"title"; }
++ (NSString *) defaultSortKey { return @"earliestDate"; }
 - (NSString *) titleInitial { return [self.title substringToIndex: 1]; }
 + (NSString *) userVisibleName { return @"comic"; }
 + (NSString *) userVisiblePluralName  { return @"comics"; }
