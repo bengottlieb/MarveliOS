2fd20f202ded534747c0a300a0b0fde24770d081
Ben Gottlieb <ben@standalone.com>
2019-06-16 20:35:59 -0500

    made Character Identifiable

--- Sources/MarveliOS/Character.swift
+++ Sources/MarveliOS/Character.swift
@@ -6,57 +6,58 @@
 //
 
 import Foundation
+import SwiftUI
 
-public struct Character: Codable {
-	let id: Int
-	let name: String
-	let description: String
-	let modified: String
-	let resourceURI: URL?
-	let urls: [URLInfo]
-	let thumbnail: ImageInfo?
-	let comics: ResourceList<ComicSummary>?
+public struct Character: Codable, Identifiable {
+	public let id: Int
+	public let name: String
+	public let description: String
+	public let modified: String
+	public let resourceURI: URL?
+	public let urls: [URLInfo]
+	public let thumbnail: ImageInfo?
+	public let comics: ResourceList<ComicSummary>?
 }
 
 public struct URLInfo: Codable {
-	let url: URL
-	let type: String
+	public let url: URL
+	public let type: String
 }
 
 public struct PayloadWrapper<PayloadData: Codable>: Codable {
-	let code: Int
-	let status: String?
-	let attributionText: String?
-	let data: PagedWrapper
-	let etag: String?
+	public let code: Int
+	public let status: String?
+	public let attributionText: String?
+	public let data: PagedWrapper
+	public let etag: String?
 	
 	public struct PagedWrapper: Codable {
-		let offset: Int
-		let limit: Int
-		let total: Int
-		let count: Int
-		let results: [PayloadData]
+		public let offset: Int
+		public let limit: Int
+		public let total: Int
+		public let count: Int
+		public let results: [PayloadData]
 	}
 }
 
 public struct ImageInfo: Codable {
-	let path: String
-	let `extension`: String
+	public let path: String
+	public let `extension`: String
 }
 
 public struct ResourceList<Element: Codable>: Codable {
-	let available: Int?
-	let returned: Int?
-	let collectionURI: URL?
-	let items: [Element]
+	public let available: Int?
+	public let returned: Int?
+	public let collectionURI: URL?
+	public let items: [Element]
 }
 
 public struct ComicSummary: Codable {
-	let resourceURI: URL?
-	let name: String!
+	public let resourceURI: URL?
+	public let name: String!
 }
 
 public struct ServerError: Codable {
-	let code: String
-	let message: String
+	public let code: String
+	public let message: String
 }
